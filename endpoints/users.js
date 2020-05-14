29e7d2013e853833b14e6050fdbe25a679403f02
Jim Cosgrave <jcosgrave@bdo.com>
2020-05-13 21:04:47 -0500

    Added subscribe/unsubscribe to store

--- endpoints/users.js
+++ endpoints/users.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var db = require('../db');
 var passwordHash = require('password-hash');
 var authentication = require('../middleware/authentication');
+const { ObjectId } = require('mongodb').ObjectId;
 
 router.use(function timeLog(req, res, next) {
     //console.log('User API called at : ', Date.now());
@@ -21,7 +22,7 @@ router.get('/:user?', function(req, res){
     });
 });
 
-router.post('/', function(req, res){
+router.post('/', function(req, res) {
     let response = {};
     const collection = getCollection();
     const filter = { username: req.body.username }
@@ -37,6 +38,70 @@ router.post('/', function(req, res){
         response.user_id = user._id;
 
         res.json(response);
+    });
+});
+
+//
+// POST - Subscribe to a store
+//
+router.post('/subscribeToStore', authentication.authenticateToken, function(req, res) {
+    const collection = getCollection();
+    const filter = { _id: new ObjectId(req.user.userId) };
+
+    collection.findOne(filter, function(err, user) {
+        if(!user) {
+            res.json({ unauthorized: true });
+            return;
+        }
+
+        const store = req.body.store;
+
+        if(!user.stores) {
+            user.stores = [];
+        }
+
+        user.stores.push(store);
+
+        var update = { $set: { stores: user.stores } };
+
+        collection.updateOne(filter, update, function (err, doc) {
+            res.json(user);
+        });
+        
+        return;
+    });
+});
+
+//
+// POST - Unsubscribe from a store
+//
+router.post('/unsubscribeFromStore', authentication.authenticateToken, function(req, res) {
+    const collection = getCollection();
+    const filter = { _id: new ObjectId(req.user.userId) };
+
+    collection.findOne(filter, function(err, user) {
+        if(!user) {
+            res.json({ unauthorized: true });
+            return;
+        }
+
+        const store = req.body.store;
+
+        if(!user.stores) {
+            res.json({ error: 'Not found' });
+            return;
+        }
+
+        let index = user.stores.map(function(s) { return s.store_id; }).indexOf(store._id.toString());
+        user.stores.splice(index, 1);
+
+        var update = { $set: { stores: user.stores } };
+
+        collection.updateOne(filter, update, function (err, doc) {
+            res.json(user);
+        });
+        
+        return;
     });
 });
 
