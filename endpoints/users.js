bfef652f8f043c4a185f2410d7b09d5017d276b9
Jim Cosgrave <jcosgrave@bdo.com>
2020-06-06 06:47:55 -0500

    Fixed an issue with authentication

--- endpoints/users.js
+++ endpoints/users.js
@@ -7,56 +7,61 @@ const { ObjectId } = require('mongodb').ObjectId;
 
 router.use(function timeLog(req, res, next) {
     //console.log('User API called at : ', Date.now());
-    next(); 
+    next();
 });
 
-const getCollection = function() {
-    return db.getCollection('users'); 
+const getCollection = function () {
+    return db.getCollection('users');
 }
 
-router.get('/:user?', function(req, res){
+router.get('/:user?', function (req, res) {
     var collection = getCollection();
 
-    collection.find().toArray(function(err, docs) {
+    collection.find().toArray(function (err, docs) {
         res.json(docs);
     });
 });
 
-router.post('/', function(req, res) {
+router.post('/', function (req, res) {
     let response = {};
     const collection = getCollection();
     const filter = { username: req.body.username }
 
-    collection.findOne(filter, function(err, user) {
-        if(!user) {
+    collection.findOne(filter, function (err, user) {
+        if (!user) {
             res.json({ unauthorized: true });
             return;
         }
 
         response.valid = passwordHash.verify(req.body.password, user.password);
-        response.token = authentication.generateAccessToken(user);
-        response.user_id = user._id;
 
-        res.json(response);
+        if (response.valid) {
+            response.token = authentication.generateAccessToken(user);
+            response.user_id = user._id;
+
+            res.json(response);
+        } else {
+            res.json({ status: 'Please enter a valid username/password' });
+        }
     });
 });
 
 //
 // POST - Subscribe to a store
 //
-router.post('/subscribeToStore', authentication.authenticateToken, function(req, res) {
+router.post('/subscribeToStore', authentication.authenticateToken, function (req, res) {
     const collection = getCollection();
     const filter = { _id: new ObjectId(req.user.userId) };
 
-    collection.findOne(filter, function(err, user) {
-        if(!user) {
+    collection.findOne(filter, function (err, user) {
+        if (!user) {
             res.json({ unauthorized: true });
             return;
         }
 
         const store = req.body.store;
 
-        if(!user.stores) {
+        if (!user.stores) {
             user.stores = [];
         }
 
@@ -67,7 +72,7 @@ router.post('/subscribeToStore', authentication.authenticateToken, function(req,
         collection.updateOne(filter, update, function (err, doc) {
             res.json(user);
         });
-        
+
         return;
     });
 });
@@ -75,24 +80,24 @@ router.post('/subscribeToStore', authentication.authenticateToken, function(req,
 //
 // POST - Unsubscribe from a store
 //
-router.post('/unsubscribeFromStore', authentication.authenticateToken, function(req, res) {
+router.post('/unsubscribeFromStore', authentication.authenticateToken, function (req, res) {
     const collection = getCollection();
     const filter = { _id: new ObjectId(req.user.userId) };
 
-    collection.findOne(filter, function(err, user) {
-        if(!user) {
+    collection.findOne(filter, function (err, user) {
+        if (!user) {
             res.json({ unauthorized: true });
             return;
         }
 
         const store = req.body.store;
 
-        if(!user.stores) {
+        if (!user.stores) {
             res.json({ error: 'Not found' });
             return;
         }
 
-        let index = user.stores.map(function(s) { return s.store_id; }).indexOf(store._id.toString());
+        let index = user.stores.map(function (s) { return s.store_id; }).indexOf(store._id.toString());
         user.stores.splice(index, 1);
 
         var update = { $set: { stores: user.stores } };
@@ -100,7 +105,7 @@ router.post('/unsubscribeFromStore', authentication.authenticateToken, function(
         collection.updateOne(filter, update, function (err, doc) {
             res.json(user);
         });
-        
+
         return;
     });
 });
